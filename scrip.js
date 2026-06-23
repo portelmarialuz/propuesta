4cf424b4d215956c842834be811b182ef5576fd9
portelmarialuz <portelmarialuz@gmail.com>
2026-06-22 21:45:49 -0300

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,35 @@
+function aceptar(){
+    document.getElementById("mensaje").innerHTML =
+    "🥰 ¡Sabía que dirías que sí! Será una cita increíble ❤️";
+}
+
+const botonNo = document.getElementById("no");
+
+botonNo.addEventListener("mouseover", () => {
+    const x = Math.random() * (window.innerWidth - 120);
+    const y = Math.random() * (window.innerHeight - 80);
+
+    botonNo.style.position = "absolute";
+    botonNo.style.left = x + "px";
+    botonNo.style.top = y + "px";
+});
+
+// Corazones flotando
+function crearCorazon(){
+    const corazon = document.createElement("div");
+
+    corazon.classList.add("corazon");
+    corazon.innerHTML = "❤️";
+
+    corazon.style.left = Math.random() * window.innerWidth + "px";
+    corazon.style.fontSize = (Math.random() * 25 + 15) + "px";
+    corazon.style.animationDuration = (Math.random() * 5 + 5) + "s";
+
+    document.body.appendChild(corazon);
+
+    setTimeout(() => {
+        corazon.remove();
+    }, 10000);
+}
+
+setInterval(crearCorazon, 300);
